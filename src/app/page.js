4b43bb0fa0f9ede52240c7cb59c84aed313edd17
Dylan Cheng <DChill0624@gmail.com>
2026-06-01 21:29:52 -0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,43 @@
-// "use client";
+"use client";
 
-// import Link from "next/link";
+import Link from "next/link";
 
-// import AuthButtons from
-//   "../components/AuthButtons.js";
+import AuthButtons from
+  "../components/AuthButtons.js";
 
-// import MiniGame from
-//   "../components/Minigame.js";
+import MiniGame from
+  "../components/Minigame.js";
 
-// import {
-//   useAuth
-// } from "../context/AuthContext.js";
-
-// export default function Home() {
-//   const { user } = useAuth();
-
-//   return (
-//     <main
-//       style={{
-//         padding: "2rem",
-//       }}
-//     >
-//       <h1>
-//         Mastery Platform
-//       </h1>
-
-//       {user ? (
-//         <Link href="/profile">
-//           Profile
-//         </Link>
-//       ) : (
-//         <AuthButtons />
-//       )}
-
-//       <hr />
-
-//       <MiniGame
-//         user={user}
-//       />
-//     </main>
-//   );
-// }
+import {
+  useAuth
+} from "../context/AuthContext.js";
 
 export default function Home() {
-  return <h1>PAGE WORKS</h1>;
+  const { user } = useAuth();
+
+  return (
+    <main
+      style={{
+        padding: "2rem",
+      }}
+    >
+      <h1>
+        Mastery Platform
+      </h1>
+
+      {user ? (
+        <Link href="/profile">
+          Profile
+        </Link>
+      ) : (
+        <AuthButtons />
+      )}
+
+      <hr />
+
+      <MiniGame
+        user={user}
+      />
+    </main>
+  );
 }
