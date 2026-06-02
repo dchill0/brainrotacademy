5bace5799ce236153fce58cd2733c574b63fbb38
Dylan Cheng <DChill0624@gmail.com>
2026-06-01 21:38:18 -0400

    test2

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,47 @@
-"use client";
+// "use client";
 
-import Link from "next/link";
+// import Link from "next/link";
 
-import AuthButtons from
-  "../components/AuthButtons.js";
+// import AuthButtons from
+//   "../components/AuthButtons.js";
 
-import MiniGame from
-  "../components/Minigame.js";
+// import MiniGame from
+//   "../components/Minigame.js";
 
-import {
-  useAuth
-} from "../context/AuthContext.js";
+// import {
+//   useAuth
+// } from "../context/AuthContext.js";
+
+// export default function Home() {
+//   const { user } = useAuth();
+
+//   return (
+//     <main
+//       style={{
+//         padding: "2rem",
+//       }}
+//     >
+//       <h1>
+//         Mastery Platform
+//       </h1>
+
+//       {user ? (
+//         <Link href="/profile">
+//           Profile
+//         </Link>
+//       ) : (
+//         <AuthButtons />
+//       )}
+
+//       <hr />
+
+//       <MiniGame
+//         user={user}
+//       />
+//     </main>
+//   );
+// }
 
 export default function Home() {
-  const { user } = useAuth();
-
-  return (
-    <main
-      style={{
-        padding: "2rem",
-      }}
-    >
-      <h1>
-        Mastery Platform
-      </h1>
-
-      {user ? (
-        <Link href="/profile">
-          Profile
-        </Link>
-      ) : (
-        <AuthButtons />
-      )}
-
-      <hr />
-
-      <MiniGame
-        user={user}
-      />
-    </main>
-  );
+  return <h1>IT WORKS</h1>;
 }
